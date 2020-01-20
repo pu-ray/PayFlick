a5a226e914c52869c737aa6d5f1789dc5e5bebd8
mbuguapurity <mbuguapurity1@gmail.com>
2020-01-20 18:51:02 +0300

    changes

--- officelunchdelivery/src/components/Footer.js
+++ officelunchdelivery/src/components/Footer.js
@@ -4,11 +4,11 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer-area fruits-footer">
-        {/* <div className="food-copyright black-bg-6 ptb-30">
+        <div className="food-copyright black-bg-6 ptb-30">
           <div className="container text-center">
             <p>Copyright © <a href="https://hastech.company/"> Greens Kiosk</a> 2019 . All Right Reserved.</p>
           </div>
-        </div>  */}
+        </div> 
       </footer>
     )
   }
